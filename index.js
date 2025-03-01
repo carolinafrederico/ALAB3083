d1c28abb8f8aadb2d48f5c9ada7ace9afd035a52
carolinafrederico <iam@carolinafrederico.com>
2025-03-01 15:04:24 -0500

    exercise 2

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 // Loop through all numbers from 1 to 100.
-for (let number = 1; number <= 100; number = number + 1) {
-  console.log(number);
+for (let n = 1; n <= 100; n = n + 1) {
+  console.log(n);
 }
+for (let n = 1; n <= 100; n++) {
+	let answer = "";
+	if (n % 3 == 0) answer += "Fizz";
+	if (n % 5 == 0) answer += "Buzz";
+	console.log(answer || n);
+  }
