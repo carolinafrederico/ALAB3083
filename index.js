1b85973268342359b38b82cd0059479efdd66b0d
carolinafrederico <iam@carolinafrederico.com>
2025-03-04 11:52:42 -0500

    Exercise 3

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-// Loop through all numbers from 1 to 100.
-for (let n = 1; n <= 100; n = n + 1) {
-  console.log(n);
-}
-for (let n = 1; n <= 100; n++) {
-	let answer = "";
-	if (n % 3 == 0) answer += "Fizz";
-	if (n % 5 == 0) answer += "Buzz";
-	console.log(answer || n);
+// // Loop through all numbers from 1 to 100.
+// for (let n = 1; n <= 100; n = n + 1) {
+//   console.log(n);
+// }
+// // log FizzBuzz
+// for (let n = 1; n <= 100; n++) {
+// 	let answer = "";
+// 	if (n % 3 == 0) answer += "Fizz";
+// 	if (n % 5 == 0) answer += "Buzz";
+// 	console.log(answer || n);
+//   }
+
+  // Feeling Loopy
+  const inputStr = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
+  const strRows = inputStr.split("\n");
+  for (let i = 0; i <= strRows.length-1; i++) {
+	const cell = strRows[i].split(",");
+	console.log(cell[0], cell[1], cell[2], cell[3]);
   }
